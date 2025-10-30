c8eaa0d72fe5297b624f5b014b8836186c5a6a57
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2025-10-30 04:59:32 +0200

    Working on the crouching animation
    Crouch animation is broken

--- UnnamedHorrorGame/Assets/Mini First Person Controller/Scripts/Components/Crouch.cs
+++ UnnamedHorrorGame/Assets/Mini First Person Controller/Scripts/Components/Crouch.cs
@@ -21,7 +21,7 @@ public class Crouch : MonoBehaviour
     public CapsuleCollider colliderToLower;
     [HideInInspector]
     public float? defaultColliderHeight;
-
+    [SerializeField] private Animator animator;
     public bool IsCrouched { get; private set; }
     public event System.Action CrouchStart, CrouchEnd;
 
@@ -81,6 +81,7 @@ public class Crouch : MonoBehaviour
             {
                 IsCrouched = true;
                 SetSpeedOverrideActive(true);
+                animator.SetBool("IsCrouching", true);
                 CrouchStart?.Invoke();
             }
         }
